a4223515bc746c6b1eede63987fa60209d3990f8
vipinmemon <vipinmemon8123@gmail.com>
2025-07-22 15:11:39 +0530

    fix: resolved react-loader-spinner build issue

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,16 +3,11 @@ import { Outlet, useNavigation } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import ThemeProvider from "./context/themecontext";
-import LoaderNavigation from "./components/Loader/LoaderNavigation";
+
 import ScrollToTop from "./components/scrollTop/ScrollTop";
 
 function Layout() {
-  // loader
-
-  const navigation = useNavigation();
-  console.log(navigation);
-
-  if (navigation.state === "loading") return <LoaderNavigation />;
+  
 
   const [themeMode, setThemeMode] = useState(() => {
     return localStorage.getItem("mode") || "light";
--- src/components/Loader/LoaderManual.jsx
+++ src/components/Loader/LoaderManual.jsx
@@ -1,25 +1,25 @@
-import { ColorRing } from "react-loader-spinner";
-import React from "react";
+// import { ColorRing } from "react-loader-spinner";
+// import React from "react";
 
-function LoaderManual({ show }) {
-  if (!show) return null;
-  return (
-    <div style={{ textAlign: "center", marginTop: "300px" }}>
-      <ColorRing
-        visible={true}
-        height="80"
-        width="80"
-        ariaLabel="color-ring-loading"
-        wrapperStyle={{}}
-        wrapperClass="color-ring-wrapper"
-        colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
-      />
-    </div>
-  );
-}
+// function LoaderManual({ show }) {
+//   if (!show) return null;
+//   return (
+//     <div style={{ textAlign: "center", marginTop: "300px" }}>
+//       <ColorRing
+//         visible={true}
+//         height="80"
+//         width="80"
+//         ariaLabel="color-ring-loading"
+//         wrapperStyle={{}}
+//         wrapperClass="color-ring-wrapper"
+//         colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+//       />
+//     </div>
+//   );
+// }
 
-//jaha bhi use karenge (manual loader whenever call api,fetch data from bakend befor on after close loader)
-//const [loading, setLoading] = useState(true);
-// <Loading show={loading} />
+// //jaha bhi use karenge (manual loader whenever call api,fetch data from bakend befor on after close loader)
+// //const [loading, setLoading] = useState(true);
+// // <Loading show={loading} />
 
-export default LoaderManual;
+// export default LoaderManual;
